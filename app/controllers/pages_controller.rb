5d6da786afd998fa42433e8abc5dee18ffe2469f
JLHayes <jhayes12@memphis.edu>
2019-01-31 00:49:53 +0000

    step 3 progress 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+  def jhayes12
+    # render jhayes12.html.erb
+  end
+
+  def propresenter
+    # render propresenter.html.erb
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'jhayes12', to: 'pages#jhayes12', as: 'jhayes12'
+  get 'propresenter', to: 'pages#propresenter', as: 'propresenter'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
